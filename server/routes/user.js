0b3c05cc652f8ebb9531752e18793afff34fc293
zakhak2 <kostia210@gmail.com>
2019-10-16 09:58:18 +0300

    add route to get user by name, change post route: add user when doesnt exist

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,11 @@ const UserModel = require("../models/user");
 
 route.use(bodyParser.json());
 
+function passCut(user) {
+    const {name, email} = user;
+    return { name, email };
+}
+
 const userValidationSchema = Joi.object().keys({
     name: Joi.string().min(2).required(),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
@@ -13,8 +18,15 @@ const userValidationSchema = Joi.object().keys({
 });
 
 
-route.get("/", (req, res) => {
-    res.send("test /user");
+route.get("/:name", (req, res) => {
+    const { name } = req.params;
+    UserModel.find({ name }, (err, data) => {
+        if (data.length) {
+            res.json(passCut(data[0]));
+        } else {
+            res.status(404).send(`can\'t find user ${name}!`);
+        }
+    });
 });
 
 route.post("/", (req, res) => {
@@ -22,11 +34,16 @@ route.post("/", (req, res) => {
         if (err) {
             res.status(400).send("bad user");
         } else {
-            new UserModel(data).save();
+            UserModel.find({name: data.name}, (err, dataArr) => {
+                if (dataArr.length) {
+                    res.status(409).send("user already exist!");
+                } else {
+                    new UserModel(data).save();
+                    res.send("user added");
+                }
+            });
         }
     });
-
-    res.send("posst");
 });
 
 module.exports = route;
